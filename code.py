72b3e4f43736141ac8a7750df7ab354a68a2055c
Remy Skelton <remy.skelton@stu.ocsb.ca>
2023-12-19 12:55:19 -0500

    Completed cpt 1

---
+++ code.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+# Created by: Remy Skelton
+# Date: Dec. 19,2023
+# This program is a "Space Alien" game on pybadge
+
+
+import stage
+import ugame
+
+def game_scene():
+    # this functionne is the code create the main game scene
+    print("\n\n\n") # creates 3 blank lines 
+    print("Hello, REMY SKELETON")
+
+
+    #a forever loop
+    while True:
+        pass # place holder
+
+if __name__ == "__main__":
+    game_scene()
