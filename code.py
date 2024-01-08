2b4659c59024e6ac512a69d128365e740dde67b7
Remy Skelton <remy.skelton@stu.ocsb.ca>
2024-01-08 12:03:18 -0500

    Done CPT-02

--- code.py
+++ code.py
@@ -4,18 +4,38 @@
 # This program is a "Space Alien" game on pybadge
 
 
+
+
 import stage
 import ugame
 
+
 def game_scene():
-    # this functionne is the code create the main game scene
-    print("\n\n\n") # creates 3 blank lines 
-    print("Hello, REMY SKELETON")
+    # this function is the code create the main game scene
 
 
+    # image banks for CircuitPython
+    image_bank_background = stage.Bank.from_bmp16("space_aliens_background.bmp")
+
+
+    # set the background to the 0 image from image bank
+    # the size will be (10x8 tiles of sixe 16x16)
+    background = stage.Grid(image_bank_background, 10, 8)
+
+
+    # create the stage for the background to show
+    # frames at 60 fps
+    game = stage.Stage(ugame.display, 60)
+    # set the layers of all the sprites, items to show in order
+    game.layers = [background]
+    # render the sprites
+    # render the game scene once per scene
+    game.render_block()
+   
     # a forever loop
     while True:
         pass # place holder
+
 
 if __name__ == "__main__":
     game_scene()
---
+++ constants.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# Created by: Remy Skelton
+# Date: Dec. 19,2023
+# This constants file for the space Alien game
+
+# screen size is 160x128 and sprite siz is 16x16
+SCREEN_X = 160
+SCREEN_Y = 128
+SCREEN_GRID_X = 10
+SCREEN_GRID_Y = 8
+SPRITE_SIZE = 16
+TOTAL_NUMBER_OF_ALIENS = 5
+FPS = 60
+SPRITE_MOVEMENT_SPEED = 1
