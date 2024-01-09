3d49e038099ab807e0a0969cb90fc9cca18d50f5
Remy Skelton <remy.skelton@stu.ocsb.ca>
2024-01-09 12:58:44 -0500

    Done CPT-06

--- code.py
+++ code.py
@@ -19,6 +19,18 @@ def game_scene():
     image_bank_background = stage.Bank.from_bmp16("space_aliens_background.bmp")
     image_bank_sprites = stage.Bank.from_bmp16("space_aliens.bmp")
 
+    # buttons that keep state information on
+    a_button = constants.button_state["button_up"]
+    b_button = constants.button_state["button_up"]
+    start_button = constants.button_state["button_up"]
+    select_button = constants.button_state["button_up"]
+
+    # sound to play
+    pew_sound = open("pew.wav", 'rb')
+    sound = ugame.audio
+    sound.stop()
+    sound.mute(False)
+
 
     # set the background to the 0 image from image bank
     # the size will be (10x8 tiles of sixe 16x16)
@@ -28,12 +40,15 @@ def game_scene():
     # a sprite that will update every frame with te background
     ship = stage.Sprite(image_bank_sprites, 5, 75, 66)
 
+    alien = stage.Sprite(image_bank_sprites, 9, int(constants.SCREEN_X / 2 - constants.SPRITE_SIZE / 2), 16)
+
 
     # create the stage for the background to show
     # frames at 60 fps
     game = stage.Stage(ugame.display, constants.FPS)
-    # set the layers of all the sprites, items to show in order
-    game.layers = [ship] + [background]
+
+    # set the layers, so the item show up in order
+    game.layers = [ship] + [alien] + [background]
     # render the sprites
     # render the game scene once per scene
     game.render_block()
@@ -45,16 +60,27 @@ def game_scene():
         keys = ugame.buttons.get_pressed()
 
 
-        if keys & ugame.K_X:
+        # A button to shoot
+        if keys & ugame.K_O != 0:
+            if a_button == constants.button_state["button_up"]:
+                a_button = constants.button_state["button_just_pressed"]
+            elif a_button == constants.button_state["button_just_pressed"]:
+                a_button = constants.button_state["button_released"]
+        else:
+            if a_button == constants.button_state["button_still_pressed"]:
+                a_button = constants.button_state["button_released"]
+            else:
+                a_button = constants.button_state["button_up"]
+
+        # B button
+        if keys & ugame.K_O != 0:
             pass
-        if keys & ugame.K_O:
+        if keys & ugame.K_START != 0:
             pass
-        if keys & ugame.K_START:
-            pass
-        if keys & ugame.K_SELECT:
+        if keys & ugame.K_SELECT != 0:
             pass
         if keys & ugame.K_RIGHT:
-            if ship.x <= 144:
+            if ship.x <= constants.SCREEN_X - constants.SPRITE_SIZE:
                 ship.move(ship.x + 1, ship.y)
             else:
                 ship.move(0, ship.y)
@@ -62,7 +88,7 @@ def game_scene():
             if ship.x >= 0:
                 ship.move(ship.x - 1, ship.y)
             else:
-                ship.move(constants.SCREEN_X - 16, ship.y)
+                ship.move(constants.SCREEN_X - constants.SPRITE_SIZE, ship.y)
         if keys & ugame.K_UP:
             if ship.y >= 0:
                 ship.move(ship.x, ship.y - 1)
@@ -74,10 +100,11 @@ def game_scene():
             else:
                 ship.move(ship.x, 120)
         # update the logic of the game
-
+        if a_button == constants.button_state["button_just_pressed"]:
+            sound.play(pew_sound)
 
         # only refresh the sprite
-        game.render_sprites([ship])
+        game.render_sprites([ship] + [alien])
         game.tick()
 
 
--- constants.py
+++ constants.py
@@ -3,7 +3,7 @@
 # Date: Dec. 19,2023
 # This constants file for the space Alien game
 
-# screen size is 160x128 and sprite siz is 16x16
+# screen size is 160x128 and sprite size is 16x16
 SCREEN_X = 160
 SCREEN_Y = 128
 SCREEN_GRID_X = 10
@@ -12,3 +12,11 @@ SPRITE_SIZE = 16
 TOTAL_NUMBER_OF_ALIENS = 5
 FPS = 60
 SPRITE_MOVEMENT_SPEED = 1
+
+# used for buttons
+button_state = {
+    "button_up": "up",
+    "button_just_pressed": "just pressed",
+    "button_still_pressed": "still pressed",
+    "button_released": "released"
+}
