95f309374e5a161c11566425716685b2ea0ddc4b
Remy Skelton <remy.skelton@stu.ocsb.ca>
2024-01-11 12:47:55 -0500

    Done CPT-10

--- code.py
+++ code.py
@@ -8,6 +8,8 @@
 
 import stage
 import ugame
+import random
+import time
 import constants
 
 
@@ -16,13 +18,13 @@ def menu_scene():
 
 
     # image banks for CircuitPython
-    image_bank_background = stage.Bank.from_bmp16("space_aliens_background.bmp")
+    image_bank_background = stage.Bank.from_bmp16("mt_game_studio.bmp")
 
     # add text object
     text = []
     text1 = stage.Text(width=29, height=12, font=None, palette=constants.RED_PALETTE, buffer=None)
-    text1.move(20,10)
-    text1.text("MT Game Studios")
+    text1.move(30,10)
+    text1.text("Messi studio")
     text.append(text1)
 
     text2 = stage.Text(width=29, height=12, font=None, palette=constants.RED_PALETTE, buffer=None)
@@ -55,6 +57,102 @@ def menu_scene():
 
         # redraw the sprites
         game.tick()
+
+
+def splash_scene():
+    # this function is the code create the main game scene
+
+
+    # sound to play
+    coin_sound = open("coin.wav", 'rb')
+    sound = ugame.audio
+    sound.stop()
+    sound.mute(False)
+    sound.play(coin_sound)
+
+
+    # image banks for CircuitPython
+    image_bank_background = stage.Bank.from_bmp16("mt_game_studio.bmp")
+
+    # set the background to the 0 image from image bank
+    # the size will be (10x8 tiles of sixe 16x16)
+    background = stage.Grid(image_bank_background, constants.SCREEN_GRID_X, constants.SCREEN_GRID_Y)
+
+   # used this program to split the image into tile: 
+
+    #   https://ezgif.com/sprite-cutter/ezgif-5-818cdbcc3f66.png
+
+    background.tile(2, 2, 0)  # blank white
+
+    background.tile(3, 2, 1)
+
+    background.tile(4, 2, 2)
+
+    background.tile(5, 2, 3)
+
+    background.tile(6, 2, 4)
+
+    background.tile(7, 2, 0)  # blank white
+
+
+
+    background.tile(2, 3, 0)  # blank white
+
+    background.tile(3, 3, 5)
+
+    background.tile(4, 3, 6)
+
+    background.tile(5, 3, 7)
+
+    background.tile(6, 3, 8)
+
+    background.tile(7, 3, 0)  # blank white
+
+
+
+    background.tile(2, 4, 0)  # blank white
+
+    background.tile(3, 4, 9)
+
+    background.tile(4, 4, 10)
+
+    background.tile(5, 4, 11)
+
+    background.tile(6, 4, 12)
+
+    background.tile(7, 4, 0)  # blank white
+
+
+
+    background.tile(2, 5, 0)  # blank white
+
+    background.tile(3, 5, 0)
+
+    background.tile(4, 5, 13)
+
+    background.tile(5, 5, 14)
+
+    background.tile(6, 5, 0)
+
+    background.tile(7, 5, 0)  # blank white
+
+    # create the stage for the background to show
+    # frames at 60 fps
+    game = stage.Stage(ugame.display, constants.FPS)
+
+    # set the layers, so the item show up in order
+    game.layers = [background]
+    # render the sprites
+    # render the game scene once per scene
+    game.render_block()
+
+
+    # a forever loop
+    while True:
+        # wait for 2 seconds to go to menu
+        time.sleep(2.0)
+        menu_scene()
+
 
 
 def game_scene():
@@ -157,4 +255,4 @@ def game_scene():
 
 
 if __name__ == "__main__":
-    menu_scene()
+    splash_scene()
